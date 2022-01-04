47927efc460cc5403144d36f707cdb726178f2c2
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-04 02:31:40 +0000

    create 6 new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,35 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Taylor", 
+  last_name: "Noj", 
+  hourly_rate: 135
+)
+@store1.employees.create(
+  first_name: "Anastassia", 
+  last_name: "Tsvetkova ", 
+  hourly_rate: 125
+)
+
+@store2.employees.create(
+  first_name: "Mei", 
+  last_name: "Nghiem", 
+  hourly_rate: 150
+)
+@store2.employees.create(
+  first_name: "Bles", 
+  last_name: "Mateo", 
+  hourly_rate: 100
+)
+@store2.employees.create(
+  first_name: "Stephanie", 
+  last_name: "Li", 
+  hourly_rate: 115
+)
+
